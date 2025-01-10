05233e82d6f80798495e6cff8a07c4f221998a15
Aniruddh Languify <aniruddh.languify@gmail.com>
2025-01-10 17:21:32 +0530

    fix():added domains for coin gecko.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['assets.coingecko.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['assets.coingecko.com', 'coin-images.coingecko.com'], // Add both domains if needed
+  },
+};
+
+export default nextConfig;
